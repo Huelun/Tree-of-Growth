4f41edd8497cb249182688d0e82ca3813e53f1f9
Wulgruf <wulgruf@protonmail.com>
2023-10-08 19:27:43 +0200

    Changes in item.py

--- cogs/item.py
+++ cogs/item.py
@@ -1,10 +1,10 @@
 class Item:
     def __init__(self, name, value):
         self.name = name
-        self.price = value
+        self.value = value
 
     def get_description(self):
-        return f"Item: {self.name}\nPrice: ${self.price:.2f}"
+        return f"Item: {self.name}\nValue: {self.value:.2f} troves"
 
 
 class Consumable(Item):
@@ -41,3 +41,17 @@ class Wieldable(Item):
 
     def get_description(self):
         return super().get_description() + f"\nDamage: {self.damage} points"
+
+
+consumable_item = Consumable("Health Potion", 5.99, "Restores 50 HP")
+material_item = Material("Iron Ingot", 1.99, "Metal")
+wearable_item = Wearable("Leather Armor", 39.99, "Medium")
+wieldable_item = Wieldable("Sword", 29.99, 20)
+
+print(consumable_item.get_description())
+print("\n")
+print(material_item.get_description())
+print("\n")
+print(wearable_item.get_description())
+print("\n")
+print(wieldable_item.get_description())
